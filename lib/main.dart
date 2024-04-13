7344e3b15ce9c700977ac75823b3dcf6df7de910
Chirag Jadhav <jadhavchirag487@gmail.com>
2024-04-13 15:47:27 +0530

    add appbar and make simple page

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 243, 236, 207),
+        appBar: AppBar(
+          title: Text('Grid View',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24),),
+          centerTitle: true,
+          elevation: 5,
+          shadowColor: Colors.black,
+          backgroundColor: Colors.amberAccent,
+        ),
+      ),
+    );
   }
 }
